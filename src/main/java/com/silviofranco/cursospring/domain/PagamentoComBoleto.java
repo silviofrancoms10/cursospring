4718097de3572e6b28d1b300b1c00753f881fde7
silviofrancoms <116981832+silviofrancoms10@users.noreply.github.com>
2022-12-13 06:22:34 -0400

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/silviofranco/cursospring/domain/PagamentoComBoleto.java
@@ -0,0 +1,38 @@
+package com.silviofranco.cursospring.domain;
+
+import jakarta.persistence.Entity;
+
+import java.util.Date;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento{
+    private static final long serialVersionUID = 1L;
+
+    private Date dataVencimento;
+    private Date dataPagamento;
+
+    public PagamentoComBoleto() {
+    }
+
+    public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
+        super(id, estado, pedido);
+        this.dataVencimento = dataVencimento;
+        this.dataPagamento = dataPagamento;
+    }
+
+    public Date getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Date dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Date getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(Date dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+}
